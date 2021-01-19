7e1b8743e9475f41edbdea31813507bb1abeba99
Ashwin <ashwin.dinesh01@gmail.com>
2021-01-19 22:10:00 +0530

    Initial commit

---
+++ app/src/main/java/com/ashwin/android/vectorimage/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ashwin.android.vectorimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        block_button.setImageResource(R.drawable.ic_block)
+    }
+}
